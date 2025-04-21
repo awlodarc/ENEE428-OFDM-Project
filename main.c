981f1884f0226deff5f2f78de6204b98e06b4971
eguzz313 <eguzzone@umd.edu>
2025-04-21 11:33:51 -0400

    Added symbol modulation code for bpsk, qpsk, 16qam

--- main.c
+++ main.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 #include "generate_sequence.h"
 #include "operations.h"
+#include "symbol_modulation.h"
+
+//training sub-carriers
+struct complex training[52] = {{1, 0}, {1, 0}, {-1, 0}, {-1, 0},
+                                {1, 0}, {1, 0}, {-1, 0}, {1, 0},
+                                {-1, 0}, {1, 0}, {1, 0}, {1, 0},
+                                {1, 0}, {1, 0}, {1, 0}, {-1, 0},
+                                {-1, 0}, {1, 0}, {1, 0}, {-1, 0},
+                                {1, 0}, {-1, 0}, {1, 0}, {1, 0},
+                                {1, 0}, {1, 0}, {1, 0}, {-1, 0},
+                                {-1, 0}, {1, 0}, {1, 0}, {-1, 0},
+                                {1, 0}, {-1, 0}, {1, 0}, {-1, 0},
+                                {-1, 0}, {-1, 0}, {-1, 0}, {-1, 0},
+                                {1, 0}, {1, 0}, {-1, 0}, {-1, 0},
+                                {1, 0}, {-1, 0}, {1, 0}, {-1, 0},
+                                {1, 0}, {1, 0}, {1, 0}, {1, 0}
+};
 
 int main() {
+    //OFDM generation
     const unsigned char* seq = get_lfsr_sequence();
     for (int i = 0; i < out_length; i++) {
         printf("%d, ", seq[i]);
     }
     printf("\n");
-    return 0;
+
+    //bpsk modulation api
+    struct complex* modulated = bpsk(seq);
 }
---
+++ symbol_modulation.h
@@ -0,0 +1,17 @@
+//
+// Created by eligu on 4/21/2025.
+//
+
+#ifndef SYMBOL_MODULATION_H
+#define SYMBOL_MODULATION_H
+
+#define bpsk_length 52
+#define qpsk_length 104
+#define sixteenqam_length 208
+
+// Functions that map input bits to modulation symbols
+struct complex* bpsk(const unsigned char* input);
+struct complex* qpsk(const unsigned char* input);
+struct complex* sixteenqam(const unsigned char* input);
+
+#endif // SYMBOL_MODULATION_H
