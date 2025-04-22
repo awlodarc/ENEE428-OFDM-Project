0c7a9fa8c807c00de9dda7b16467337225b28362
awlodarc <awlodarc@umd.edu>
2025-04-22 13:45:47 -0400

    New Add_CP file and maybe changes in FFT/IFFT i forget

---
+++ Add_CP.c
@@ -0,0 +1,19 @@
+//
+// Created by a_wlo on 4/21/2025.
+//
+
+#include "Add_CP.h"
+#include "operations.h"
+#include "FFT.h"
+
+void Add_CP(struct complex* output, struct complex* input) {
+    for (int i = 0; i < CP_LENGTH; i++) {
+        output[i] = input[FFT_SIZE - CP_LENGTH + i];
+    }
+
+    for (int i = 0; i < FFT_SIZE + CP_LENGTH; i++) {
+        output[i + CP_LENGTH] = input[i];
+    }
+
+
+}
--- main.c
+++ main.c
@@ -4,18 +4,17 @@
 #include "symbol_modulation.h"
 #include "FFT.h"
 #include "IFFT.h"
+#include "Add_CP.h"
 
 int main() {
     struct complex fft_freq_samples[FFT_SIZE];
     struct complex ifft_time_samples[FFT_SIZE];
-    struct complex fft_time_samples[FFT_SIZE] = {{1.1016e-02 , 0.0000e+00},
-{-9.3180e-01 , 0.0000e+00},
-{9.9314e-01 , 0.0000e+00},
-{-1.3645e-01 , 0.0000e+00},
-{-7.3399e-01 , 0.0000e+00},
-{1.4799e+00 , 0.0000e+00},
-{7.4623e-01 , 0.0000e+00},
-{7.2514e-01 , 0.0000e+00}};
+    struct complex fft_time_samples[FFT_SIZE];
+    for (int i = 0; i < FFT_SIZE; i++) {
+        fft_time_samples[i].real = i;
+        fft_time_samples[i].imag = 0;
+    }
+    struct complex TX_Symbol[FFT_SIZE + CP_LENGTH];
   
   
     //training sub-carriers
@@ -34,6 +33,7 @@ int main() {
                                 {1, 0}, {1, 0}, {1, 0}, {1, 0}
     };
 
+
     revOrder();
 
     twiddleFactorsFFT();
@@ -46,18 +46,24 @@ int main() {
     //     printf("real: %f imag: %f\n", fft_freq_samples[p].real, fft_freq_samples[p].imag);
     // }
 
-    for (int p = 0; p <= FFT_SIZE - 1; p++) {
-        printf("real: %f imag: %f\n", ifft_time_samples[p].real, ifft_time_samples[p].imag);
-    }
+    // for (int p = 0; p <= FFT_SIZE - 1; p++) {
+    //     printf("real: %f imag: %f\n", ifft_time_samples[p].real, ifft_time_samples[p].imag);
+    // }
 
 
     //OFDM generation
     const unsigned char* seq = get_lfsr_sequence();
     for (int i = 0; i < out_length; i++) {
-        printf("%d, ", seq[i]);
+        //printf("%d, ", seq[i]);
     }
         //bpsk modulation api
         struct complex* modulated = bpsk(seq);
+
+    Add_CP(TX_Symbol, fft_time_samples);
+
+    for (int i = 0; i < FFT_SIZE + CP_LENGTH; i++) {
+        printf("TX_Symbol[%d] = (%f, %f)\n", i, TX_Symbol[i].real, TX_Symbol[i].imag);
+    }
 
         return 0;
 }
